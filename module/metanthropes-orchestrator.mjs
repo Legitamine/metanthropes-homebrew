6b93793a77fdb1f49dbbfbf38eaee6fd5f63e17d
Lefteris Vladimiros <spirit@siradio.fm>
2023-12-21 04:54:08 +0200

    Initial Release

---
+++ module/metanthropes-orchestrator.mjs
@@ -0,0 +1,48 @@
+/**
+ * Metanthropes Early Access Orchestrator Module for Foundry VTT
+ * Author: qp aka The Orchestrator
+ *
+ * Throughtout this project, I use the following syntax for comments:
+ ** //! Marks a special comment that stands out (in Red) for critical notes.
+ ** //* Marks a comment that is used as a section header (in Green) for better visibility.
+ ** //? Marks a comment that is used for elaborating my intent (in Blue) for better readability.
+ ** //todo Marks a comment that is used for marking (in Orange) potential optimization notes.
+ *
+ * To get automatic colloring for these comments in VSCode, you can use this extension:
+ * aaron-bond.better-comments
+ *
+ */
+Hooks.once("init", async function () {
+	//? Orchestrator Module Settings
+	game.settings.register("metanthropes", "metaOrchestrator", {
+		name: "Enable Orchestrator Features",
+		hint: `
+			Enable this setting to gain access to the Orchestrator Module features.
+			`,
+		scope: "world", //? This specifies if it's a client-side setting
+		config: true, //? This makes the setting appear in the module configuration
+		requiresReload: true, //? If true, a client reload (F5) is required to activate the setting
+		type: Boolean,
+		default: true,
+		onChange: (value) => {
+			//? Do something when the setting is changed, if necessary
+		},
+	});
+	//? Beta Features Testing
+	game.settings.register("metanthropes", "metaBetaTesting", {
+		name: "Enable Beta Testing of New Features",
+		hint: `
+		Enable this setting to test New Features that are still in development.
+		These features may not be fully functional and are subject to change during development.
+		Make sure you backup your world before enabling this setting - just to be safe.
+		`,
+		scope: "world", //? This specifies if it's a client-side setting
+		config: true, //? This makes the setting appear in the module configuration
+		requiresReload: true, //? If true, a client reload (F5) is required to activate the setting
+		type: Boolean,
+		default: false,
+		onChange: (value) => {
+			//? Do something when the setting is changed, if necessary
+		},
+	});
+});
